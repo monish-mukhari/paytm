8b5565defbf1b21963196df44622d096bed5f165
Monish <monishmukhari@gmail.com>
2024-07-02 23:56:49 +0530

    refactor(lint)

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -42,6 +42,7 @@ app.post("/hdfcWebhook", async (req, res) => {
         res.json({
             message: "Captured"
         })
+        
     } catch(e) {
         console.error(e);
         res.status(411).json({
